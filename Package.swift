f978e382096e6ee02b005d10e1595a20c82886d6
manish yadav <yadavmanish89@gmail.com>
2023-08-23 18:48:04 -0500

    commit 2

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "MyLibrary"),
-        .target(name: "SuperPackage",
-                dependencies: ["MyLibrary"]),
+        .target(name: "MyLibrary", dependencies: ["MyLibrary"]),
         .target(
             name: "SuperPackage",
             dependencies: []),
