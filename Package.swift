c8ca10f838e28c5f87be52728c5cf324242f8496
manish yadav <yadavmanish89@gmail.com>
2023-08-24 09:42:55 -0500

    added NewPackage

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SuperPackage",
-            targets: ["SuperPackage"]),
+            targets: ["SuperPackage", "NewPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,11 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SuperPackage",
-            dependencies: []),
-        .target(
-            name: "NewPackage",
-            dependencies: [],
-            path: "/Sources/NewPackage"),
+            dependencies: ["NewPackage"], path: "Sources/"),
         .testTarget(
             name: "SuperPackageTests",
             dependencies: ["SuperPackage"]),
