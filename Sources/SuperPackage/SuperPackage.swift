824478f7ebba979f6d96ab038af294a63e088c6f
manish yadav <yadavmanish89@gmail.com>
2023-08-27 22:31:10 -0500

    deleted SuperPAckage

--- Sources/SuperPackage/SuperPackage.swift
+++
@@ -1,9 +0,0 @@
-public struct SuperPackage {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-    public func getPackageName() -> String {
-        return "My SuperPackage"
-    }
-}
