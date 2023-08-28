412870957d82ffc5a6a37ecd4e5bf13dd3123d05
manish yadav <yadavmanish89@gmail.com>
2023-08-27 19:12:36 -0500

    name fixed

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SuperPackageDemo",
-            targets: ["SuperPackage2"]),
+            targets: ["SuperPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
