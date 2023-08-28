b98223e4293175c31a5df6c4c8e046ad25cd1b5f
manish yadav <yadavmanish89@gmail.com>
2023-08-27 19:53:24 -0500

    remove superPackage

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SuperPackageDemo",
-            targets: ["SuperPackage"]),
-        .library(
             name: "PackageA",
             targets: ["PackageA"])
     ],
@@ -21,12 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SuperPackage",
-            dependencies: []),
-        .testTarget(
-            name: "SuperPackageTests",
-            dependencies: ["SuperPackage"]),
         .target(name: "PackageA",
                dependencies: [],
                path: "PackageA/Sources/PackageA")
